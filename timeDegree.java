ffc8c60efdca9d658452bf944d00b2b2ccdb46b4
Akshay03x <101812659+Akshay03x@users.noreply.github.com>
2022-06-26 18:02:18 +0530

    Update timeDegree.java

--- timeDegree.java
+++ timeDegree.java
@@ -1,23 +1,30 @@
-import java.util.Scanner;
-public class timeDegree{
+import java.io.*;
+
+public class angleBetweenTwoHands{
+	public static int timeDegree(double h,double m){
+		if (h<0 || m<0 ||m>60 ||h>12) {
+			System.out.println("Wrong Input");
+		}
+		if (h==12) {
+			h=0;
+		}
+		if (m==60) {
+				m=0;
+				h = h + 1;
+		}
+		if (h>12) {
+			h = h - 12;
+		}
+		int h_angle = (int)(0.5*(h*60+m));
+		int m_angle = (int)(6*m);
+
+		int angle = Math.abs(h_angle - m_angle);
+
+		angle = Math.min(360-angle,angle);
+
+		return angle;
+	}
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-
-		System.out.println("enter hour:");
-		int h=sc.nextInt();
-		System.out.println("enter minut:");
-		int m=sc.nextInt();
-
-		if(h==12){h=0;}
-		if(m==60){m=0;}
-
-		int hAngle,mAngle;
-
-		hAngle=(h*30)+(m/2);
-		mAngle=(m*6);
-
-		int angle=Math.abs(hAngle- mAngle);
-		System.out.println("angle :"+angle);
-
+		System.out.println(calculateAngle(9,30)+" degree");
 	}
 }
