4b53b2692cffaf1f5aabd9f5e8833a87f71bb867
Akshay03x <101812659+Akshay03x@users.noreply.github.com>
2022-07-14 10:35:16 +0530

    Update evalutionPostfix.java

--- LAB-5/evalutionPostfix.java
+++ LAB-5/evalutionPostfix.java
@@ -1,38 +1,9 @@
 import java.util.*;
 public class evalutionPostfix {
-    static Stack<Integer> stack = new Stack<Integer>();
 
+    static Stack<Integer> stack = new Stack<Integer>();
     public static void stackPush(int x){
-        if(x==48){
-            stack.push(0);
-        }
-        else if(x==49){
-            stack.push(1);
-        }
-        else if(x==50){
-            stack.push(2);
-        }
-        else if(x==51){
-            stack.push(3);
-        }
-        else if(x==52){
-            stack.push(4);
-        }
-        else if(x==53){
-            stack.push(5);
-        }
-        else if(x==54){
-            stack.push(6);
-        }
-        else if(x==55){
-            stack.push(7);
-        }
-        else if(x==56){
-            stack.push(8);
-        }
-        else{
-            stack.push(9);
-        }
+        stack.push(x);
     }
 
     public static void operate(int x){
@@ -66,14 +37,14 @@ public class evalutionPostfix {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     String st=sc.next();
-    // stack.push(0);
     for(int i=0;i<st.length();i++){
         int x=st.charAt(i);
         if(x==45 ||x==42||x==43||x==47){
             operate(x);
         }
         else{
-            stackPush(x);
+            int temp=(int)x-48;
+            stackPush(temp);
         }
     }
     System.out.println(stack.pop());
