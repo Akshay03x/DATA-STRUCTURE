413f12df92ceb53e7998d89715c85b5a753bb9ec
Akshay03x <101812659+Akshay03x@users.noreply.github.com>
2022-08-09 19:11:41 +0530

    Add files via upload

---
+++ LAB-11/circular_doubly_LL.java
@@ -0,0 +1,84 @@
+public class circular_doubly_LL{
+	Node head,tail;
+	public class Node{
+		int data;
+		Node prev;
+		Node next;
+		public Node(int data){
+			this.data=data;	
+			prev=null;
+			next=null;
+		}
+	}
+
+	public void insertLast(int x){
+		Node nnode=new Node(x);
+		if(head==null){
+			head=nnode;
+			tail=nnode;
+			return;
+		}
+		tail.next=nnode;
+		nnode.prev=tail;
+		tail=nnode;
+		tail.next=head;
+		head.prev=tail;
+	}
+
+	public void insertFirst(int x){
+		Node nnode=new Node(x);
+		if(head==null){
+			head=nnode;
+			tail=nnode;
+			return;
+		}
+		nnode.next=head;
+		head.prev=nnode;
+		head=nnode;
+		head.prev=tail;
+		tail.next=head;
+	}
+
+
+	public void displayFTOL(){
+		Node node=head;
+		while(node!=tail){
+			System.out.print("["+node.data+"]");
+			node=node.next;
+		}
+	}
+	public void displayLTOF(){
+		Node node=tail;
+		while(node!=null){
+			System.out.print("["+node.data+"]");
+			node=node.prev;
+		}
+	}
+
+	public int  size(){
+		int count=0;
+		Node node=head;
+		while(node!=null){
+			count++;
+			node=node.next;
+		}
+		return count;
+	}
+	public void circular(){
+		System.out.println(tail.next.data);
+		System.out.println(tail.next.next.data);
+	}
+	public static void main(String[] args) {
+		circular_doubly_LL d=new circular_doubly_LL();
+		d.insertLast(10);
+		d.insertLast(20);
+		d.insertLast(30);
+		d.insertLast(40);
+
+		d.insertFirst(0);
+		d.circular();
+		d.displayFTOL();
+
+		System.out.println();
+	}
+}
