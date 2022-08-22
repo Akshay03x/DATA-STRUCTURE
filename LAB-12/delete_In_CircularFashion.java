5bb8a6bfd8e6cbc13b89aa4f62e3601a222d35e9
Akshay03x <101812659+Akshay03x@users.noreply.github.com>
2022-08-22 21:22:12 +0530

    Add files via upload

---
+++ LAB-12/delete_In_CircularFashion.java
@@ -0,0 +1,60 @@
+public class delete_In_CircularFashion {
+    Node head,tail;
+
+    public class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            next=null;
+        }
+    }
+
+    public void insert(int x){
+        Node node=new Node(x);
+        if(head==null){
+            head=node;
+            tail=node;
+            tail.next=head;
+            return;
+        }
+        tail.next=node;
+        tail=node;
+        tail.next=head;
+    }
+
+    public void delete_In_Circular(int z){
+        Node node=head;
+        Node prev=head;
+        while(node!=node.next){
+            for(int i=z;i>1;i--){
+                prev=node;
+                node=node.next;
+            }
+            prev.next=node.next;
+            node=prev.next;
+        }
+        head=node;
+    }
+    public void display(){
+        Node node=head;
+        System.out.print("["+node.data+"]");
+        node=node.next;
+        while(node!=head){
+            System.out.print("["+node.data+"]");
+            node=node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        delete_In_CircularFashion d=new delete_In_CircularFashion();
+        d.insert(10);
+        d.insert(20);
+        d.insert(30);
+        d.insert(40);
+        d.insert(50);
+        d.delete_In_Circular(4);
+        d.display();
+    }
+
+}
