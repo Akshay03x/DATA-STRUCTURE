66b47dbb020068cdd9990e8df804a3df7ad4620c
Akshay03x <101812659+Akshay03x@users.noreply.github.com>
2022-09-07 22:11:41 +0530

    Update BinarySerchTree.java

--- LAB-15/BinarySerchTree.java
+++ LAB-15/BinarySerchTree.java
@@ -1,16 +1,15 @@
 import java.util.Stack;
-public class BinarySerchTree<Type extends Comparable<Type>>{
+public class BinarySerchTree{
 	Node root;
 	public class Node{
-		Type data;
-		Node right,left;	
-		public Node(Type data){
+		int data;
+		Node right,left;
+		public Node(int data){
 			this.data=data;
 			right=left=null;
 		}
 	}
-
-	public void insert(Type x){
+	public void insert(int x){
 		Node node=new Node(x);
 		if(root==null){
 			root=node;
@@ -19,10 +18,10 @@ public class BinarySerchTree<Type extends Comparable<Type>>{
 		Node temp=root;
 
 		while(temp.left!=null||temp.right!=null){
-			if((node.data).compareTo(temp.data)>0 && temp.right!=null){
+			if(node.data>=temp.data && temp.right!=null){
 				temp=temp.right;
 			}
-			else if((node.data).compareTo(temp.data)<0&& temp.left!=null){
+			else if(node.data<temp.data && temp.left!=null){
 				temp=temp.left;
 			}
 
@@ -30,14 +29,13 @@ public class BinarySerchTree<Type extends Comparable<Type>>{
 				break;
 			}
 		}
-		if((node.data).compareTo(temp.data)>0){
+		if(node.data>=temp.data){
 			temp.right=node;
 		}
 		else{
 			temp.left=node;
 		}
 	}
-
 	public void display(){
 		if(root==null){
 			System.out.print("null");
@@ -49,78 +47,118 @@ public class BinarySerchTree<Type extends Comparable<Type>>{
 		while(temp!=null||st.size()>0){
 			while(temp!=null){
 				st.push(temp);
-				temp=temp.left; 
+				temp=temp.left;
 			}
 			temp=st.pop();
-			System.out.print("["+temp.data+"]");
+			System.out.print("["+temp.data+"] ");
 			temp=temp.right;
 		}
 		System.out.println();
 	}
-
-
-	public void preorder() {
-        if (root == null) {
-            System.out.println("Tree is empty");
-            return;
-        }
-        Stack<Node> stack = new Stack<>();
-        stack.push(root);
-        while (!stack.isEmpty()) {
-            Node temp = stack.pop();
-            System.out.print("[" + temp.data + "] ");
-            if (temp.right != null)
-                stack.push(temp.right);
-            if (temp.left != null)
-                stack.push(temp.left);
-        }
-        System.out.println();
-    }
-
-    public void postorder() {
-        if (root == null) {
-            System.out.println("Tree is empty");
-            return;
-        }
-        Stack<Node> stack1 = new Stack<>();
-        Stack<Node> stack2 = new Stack<>();
-        stack1.push(root);
-
-        while (!stack1.empty()) {
-            Node temp = stack1.pop();
-            stack2.push(temp);
-            if (temp.left != null)
-                stack1.push(temp.left);
-            if (temp.right != null)
-                stack1.push(temp.right);
-        }
-
-        while (!stack2.empty()) {
-            Node temp = stack2.pop();
-            System.out.print("[" + temp.data + "] ");
-        }
-        System.out.println();
-    }
-
-	public static void main(String[] args) {
-		BinarySerchTree<Integer> b=new BinarySerchTree<>();
-
-		b.insert(50);
-		b.insert(25);
-		b.insert(75);
-		b.insert(22);
-		b.insert(40);
-		b.insert(60);
-		b.insert(80);
-		b.insert(90);
-		b.insert(15);
-		b.insert(30);
-
-		System.out.print("inorder");
-		b.display();
-		System.out.print("post order");
-		b.postorder();
-		System.out.print("preorder");
-		b.preorder();
+	public void preOrder(){
+		if(root==null){
+			System.out.print("empty");
+			return;
+		}
+		Stack<Node> st=new Stack<>();	
+		st.push(root);
+		while(st.size()!=0){
+			Node temp=st.pop();
+			System.out.print("["+temp.data+"] ");
+			if(temp.right!=null){
+				st.push(temp.right);
+			}
+			if(temp.left!=null){
+				st.push(temp.left);
+			}
+		}
+		System.out.println();
 	}
-}
+
+	public void postOrder(){
+		if(root==null){
+			System.out.println("empty");
+			return;
+		}
+		Stack<Node> s1=new Stack<>();
+		Stack<Node> s2=new Stack<>();
+		s1.push(root);
+		while(s1.size()>0){
+			Node temp=s1.pop();
+			s2.push(temp);
+
+			if(temp.left!=null){
+				s1.push(temp.left);
+			}
+			if(temp.right!=null){
+				s1.push(temp.right);
+			}
+		}
+
+		while(s2.size()!=0){
+			Node temp=s2.pop();
+			System.out.print("["+temp.data+"] ");
+		}
+		System.out.println();
+		
+	}
+
+
+	public void delete(int x){
+		Node parent=null;
+		Node temp=root;
+		while(temp!=null&& temp.data!=x){
+			parent=temp;
+			if(temp.data<x){
+				temp=temp.right;
+			}
+			else{
+				temp=temp.left;
+			}
+		}
+		if(temp.left==null&&temp.right==null){
+			if(parent.data>temp.data){
+				parent.left=null;
+			}
+			else{
+				parent.right=null;
+			}
+		}
+		if(temp.left==null || temp.right!=null){
+			temp=temp.right;
+			if(temp.data>=parent.data){
+				parent.right=temp;
+			}
+			else{
+				parent.left=temp;
+			}
+		}
+		if(temp.left!=null || temp.right==null){
+			temp=temp.left;
+			if(temp.data<=parent.data){
+				parent.left=temp;
+			}
+			else{
+				parent.right=temp;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		BinarySerchTree b=new BinarySerchTree();
+
+		b.insert(5);
+		b.insert(3);
+		b.insert(2);
+		b.insert(4);
+		b.insert(8);
+		b.insert(10);
+		b.insert(9);
+		// b.delete(10);
+		System.out.print("pre order:");
+		b.preOrder();
+		System.out.print("post order :");
+		b.postOrder();
+		System.out.print("inOrder :");
+		b.display();
+	}
+}    
